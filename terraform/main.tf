c88e7b3db89676a15204d20b5dd7f472707b5047
¨Rita-tech-ally¨ <ritudogra586@gmail.in>
2026-07-07 19:59:57 +0530

    Fix ansible.cfg control_path interpolation syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,6 +69,6 @@ pipelining = True
 
 [ssh_connection]
 ssh_args = -o ControlMaster=auto -o ControlPersist=30m -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -i sakshi.pem -o StrictHostKeyChecking=no -o ControlMaster=auto -o ControlPersist=30m -W %h:%p -q ubuntu@${module.compute.bastion_public_ip}"
-control_path = %%(directory)s/ansible-ssh-%%h-%%p-%%r
+control_path = %(directory)s/ansible-ssh-%%h-%%p-%%r
 EOT
 }
